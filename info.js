1b070c1d60608d71a15e7575eb93495126f511f8
shaghayeghpourghanbar <159025968+shaghayeghpourghanbar@users.noreply.github.com>
2025-12-05 22:07:10 +0330

    Add files via upload

---
+++ info.js
@@ -0,0 +1,8 @@
+export const info = [
+    {
+      pfp: "pfp.jpg",
+      name: "شقایق پورقنبر",
+      bio: "",
+      number: "09305129802",
+    },
+  ];
---
+++ links.js
@@ -0,0 +1,14 @@
+export const links = [
+    {
+        link: "https://www.instagram.com/ai.shaghayegh",
+        icon: "bi bi-instagram",
+    },
+    {
+        link: "https://t.me/ai_shaghayegh",
+        icon: "bi bi-telegram",
+    },
+    {
+        link: "https://discord.gg/",
+        icon: "bi bi-discord",
+    },
+]
